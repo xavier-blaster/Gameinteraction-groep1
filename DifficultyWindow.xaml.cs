aa83acd46425607b1ab0c72a8741dfce3116bc8b
Yekai <yekai.yan1@student.nhlstenden.com>
2025-10-27 17:29:18 +0100

    ik heb difficultyscherm grotendeels af(interface+ logica), alleen bij mothoden moet ik nog juiste veensternamen invullen.

---
+++ DifficultyWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Gameinteraction_groep1
+{
+    /// <summary>
+    /// DifficultyWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class DifficultyWindow : Window
+    {
+        public DifficultyWindow()
+        {
+            InitializeComponent();
+
+            knop3x3.Click += knop3x3methode;               // verbind knopen aan methoden, zodat je op deze knop klikt, wordt deze methode gestart.
+            knop4x4.Click += knop4x4methode;
+            knop5x5.Click += knop5x5methode;
+
+            homeknop.Click += homeknopmethode;
+            scoreknop.Click += scoreknopmethode;
+            uitlegknop.Click += uitlegknopmethode;
+
+        }
+
+        private void knop3x3methode (object sender, RoutedEventArgs e)    //knop3x3methode
+        {
+            GameWindow3x3 game3x3 = new GameWindow3x3 ();         // GameWindow3x3 later vervangen met echt vensternaam,  variabelnaam die zelf kan kiezen,  new + vensternaam.
+            game3x3.Show();               // opent dat venster
+            this.Close ();
+        }
+
+        private void knop4x4methode(object sender, RoutedEventArgs e)    //knop4x4methode
+        {
+            GameWindow4x4 game4x4 = new GameWindow4x4();         
+            game4x4.Show();
+            this.Close();
+        }
+
+        private void knop5x5methode(object sender, RoutedEventArgs e)    //knop5x5methode
+        {
+            GameWindow5x5 game5x5 = new GameWindow5x5();
+            game5x5.Show();
+            this.Close();
+        }
+
+        private void homeknopmethode (object sender, RoutedEventArgs e)    //homeknopmethode
+        {
+            menuwindow terugnaarhome = new menuwindow;
+            terugnaarhome4.Show();
+            this.Close();
+        }
+
+        private void scoreknopmethode(object sender, RoutedEventArgs e)    // scoreknopmethode
+        {
+            scorewindow terugnaarscore = new scorewindow;
+            terugnaarscore.Show();
+            this.Close();
+        }
+
+
+        private void uitlegknopmethode(object sender, RoutedEventArgs e)    // uitlegknopmethode
+        {
+            uitlegwindow terugnaaruitleg = new uitlegwindow;
+            terugnaaruitleg.Show();
+            this.Close();
+        }
+
+    }
+}
